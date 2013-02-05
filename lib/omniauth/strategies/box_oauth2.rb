a5ca3efecac92febb44e19b6e5979b2c69571357
Schuyler Ullman <schuyler.ullman@gmail.com>
2013-02-05 09:39:29 -0800

    Optionally check for direct link support.
    This is incredibly annoying. There's no info on the user object that indicates
    whether or not direct links are supported. The closest you could maybe get is
    inferring based on the total quota size, but that would still be wrong for some
    enterprise users. And the share request won't fail if the user can't use
    direct links, because technically the share still succeeded, it just won't
    work with the direct download URL that they so happily provide.
    
    So, during the information gathering, we create a new file, share that file,
    and then try to download that file. If it works, then direct links are
    supported. Regardless, we delete the test file.
    
    We need this info, hence the hoop jumping, but it adds a lot of overhead that
    others may not need. So this extra check is disabled by default and can be
    enabled with an option during the provider configuration.

--- lib/omniauth/strategies/box_oauth2.rb
+++ lib/omniauth/strategies/box_oauth2.rb
@@ -9,8 +9,14 @@ module OmniAuth
         :authorize_url => 'https://api.box.com/oauth2/authorize',
         :token_method => :post,
         :token_url => 'https://api.box.com/oauth2/token',
-        :param_name => 'code'
+        :param_name => 'code',
+        :connection_build => (Proc.new do |builder|
+          builder.request :multipart
+          builder.request :url_encoded
+          builder.adapter :net_http
+        end)
       }
+      option :check_direct_link_support, false
 
       def callback_url
         super.sub('http:', 'https:')
@@ -36,7 +42,45 @@ module OmniAuth
       end
 
       def raw_info
-        @raw_info ||= access_token.get('users/me').parsed
+        @raw_info ||= begin
+          hash = access_token.get('users/me').parsed
+          hash['direct_links'] = false
+          # There's no information in the user object that says whether or not
+          # the user's account supports direct links
+          if options[:check_direct_link_support]
+            begin
+              # Create a test file to share
+              test_filename = "direct_link_test_#{Time.current.to_i}"
+              io = StringIO.new("test")
+              payload = { :filename => Faraday::UploadIO.new(io, "text/plain", test_filename), :parent_id => 0 }
+              resp = access_token.post('files/content', {:body=>payload})
+              if resp.status == 201
+                # Now try to share that file.
+                file_info = resp.parsed['entries'].first
+                payload = { :shared_link => {
+                    # :unshared_at => Time.current.tomorrow.iso8601,
+                    :access => 'open'
+                  }}
+                resp = access_token.put("files/#{file_info['id']}", {:body=>payload.to_json})
+                if resp.status == 200
+                  file_info = resp.parsed
+                  begin
+                    # Check to see if the file can actually be downloaded
+                    download_url = file_info['shared_link']['download_url']
+                    client.request(:head, download_url)
+                    hash['direct_links'] = true
+                  rescue
+                    # Ignore, this should mean we got a 403 and direct links aren't supported.
+                  end
+                  # Delete the temp file
+                  access_token.delete("files/#{file_info['id']}")
+                end
+              end
+            rescue
+            end
+          end
+          hash
+        end
       end
 
     end
