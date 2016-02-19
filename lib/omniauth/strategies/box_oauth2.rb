282f3d37657c40b71882e8d46dcb69d24f21165d
Gregory <g.marcilhacy@gmail.com>
2016-02-18 21:56:50 -0800

    add scope param

--- lib/omniauth/strategies/box_oauth2.rb
+++ lib/omniauth/strategies/box_oauth2.rb
@@ -18,6 +18,16 @@ module OmniAuth
       }
       option :check_direct_link_support, false
 
+      def authorize_params
+        super.tap do |params|
+          %w[scope].each do |v|
+            if request.params[v]
+              params[v.to_sym] = request.params[v]
+            end
+          end
+        end
+      end
+
       def callback_url
         super.sub('http:', 'https:')
       end
